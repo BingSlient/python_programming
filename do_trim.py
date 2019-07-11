6718f3fc0186ffd1396b339f225926595638fdbd
jaylen_west <jaylen_west@foxmail.com>
2019-07-12 01:06:05 +0800

    add do_trim

---
+++ do_trim.py
@@ -0,0 +1,45 @@
+def trim(s):
+    if not s:    # if string s is empty, return s
+        return s
+    start = 0
+    end = -1
+    for c in s:
+        if c != ' ':
+            break
+        else:
+            start += 1
+    
+    for c in s[::-1]:
+        if c != ' ':
+            break
+        else:
+            end -= 1
+
+    if (start == len(s)) | (end == -(len(s) + 1)): # s is all spaces
+        return ""
+    else:
+        return s[start: (len(s)+end+1)]
+
+if __name__ == "__main__":
+    # 测试:
+    # print(trim('  hello'))
+    if trim('hello  ') != 'hello':
+        print('1: ', len(trim('hello  ')))
+        print('测试失败!')
+    elif trim('  hello') != 'hello':
+        print('2: ', len(trim('  hello')))
+        print('测试失败!')
+    elif trim('  hello  ') != 'hello':
+        print('测试失败!')
+    elif trim('  hello  world  ') != 'hello  world':
+        print('测试失败!')
+    elif trim('') != '':
+        print('测试失败!')
+    elif trim('    ') != '':
+        print('测试失败!')
+    else:
+        print('测试成功!')
+
+    
+        
+        
